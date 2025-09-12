c6796ca26e16214ac1a6c22594cbf84d15c63342
Bernard301094 <bernard30101994@gmail.com>
2025-09-12 16:55:40 -0300

    Adicionar classe MovimentoIndisponivelException para tratamento de erros de movimentos

---
+++ src/com/pokebattle/exception/MovimentoIndisponivelException.java
@@ -0,0 +1,7 @@
+package com.pokebattle.exception;
+
+public class MovimentoIndisponivelException extends PokemonException {
+    public MovimentoIndisponivelException(String message) {
+        super(message);
+    }
+}
