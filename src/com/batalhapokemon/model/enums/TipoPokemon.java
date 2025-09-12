e083313e58a18328de15d162b98c1bcf8511dea9
Bernard301094 <bernard30101994@gmail.com>
2025-09-12 20:10:35 -0300

    Adicionar método para calcular efetividade entre tipos de Pokémon

--- src/com/batalhapokemon/model/enums/TipoPokemon.java
+++ src/com/batalhapokemon/model/enums/TipoPokemon.java
@@ -3,4 +3,8 @@ package com.batalhapokemon.model.enums;
 public enum TipoPokemon {
 
     FOGO, AGUA, PLANTA, ELETRICO, VOADOR, NORMAL;
+
+    public double calcularEfetividade(TipoPokemon defensor) {
+        return 0;
+    }
 }
