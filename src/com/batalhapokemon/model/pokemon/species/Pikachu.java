227942b00ff4d63cbee9692324cdeee4b80b83ab
Bernard301094 <bernard30101994@gmail.com>
2025-09-12 19:48:06 -0300

    Adicionar classe Pikachu como uma nova espécie de Pokémon

---
+++ src/com/batalhapokemon/model/pokemon/species/Pikachu.java
@@ -0,0 +1,12 @@
+package com.batalhapokemon.model.pokemon.species;
+
+import com.batalhapokemon.model.enums.TipoPokemon;
+import com.batalhapokemon.model.pokemon.Pokemon;
+
+public class Pikachu extends Pokemon {
+
+    public Pikachu(String nome, int nivel, TipoPokemon tipo) {
+        super(nome, nivel, tipo);
+    }
+
+}
