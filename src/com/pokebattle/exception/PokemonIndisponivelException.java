ddb3a9642cfec1a84e9a566e37796ee3c5056ca3
Bernard301094 <bernard30101994@gmail.com>
2025-09-12 16:55:48 -0300

    Adicionar classe PokemonIndisponivelException para tratamento de erros de Pokémon indisponíveis

---
+++ src/com/pokebattle/exception/PokemonIndisponivelException.java
@@ -0,0 +1,7 @@
+package com.pokebattle.exception;
+
+public class PokemonIndisponivelException extends PokemonException {
+    public PokemonIndisponivelException(String message) {
+        super(message);
+    }
+}
