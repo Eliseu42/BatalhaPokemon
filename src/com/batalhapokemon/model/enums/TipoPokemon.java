ec5e85a9f1ccbba9d533e2401a94752e9e93eaac
Bernard301094 <bernard30101994@gmail.com>
2025-09-14 14:23:35 -0300

    Adicionar método para calcular efetividade entre tipos de Pokémon

--- src/com/batalhapokemon/model/enums/TipoPokemon.java
+++ src/com/batalhapokemon/model/enums/TipoPokemon.java
@@ -5,6 +5,59 @@ public enum TipoPokemon {
     FOGO, AGUA, PLANTA, ELETRICO, VOADOR, NORMAL;
 
     public double calcularEfetividade(TipoPokemon defensor) {
-        return 0;
+        switch (this) {
+            case FOGO:
+                switch (defensor) {
+                    case PLANTA:
+                        return 2.0;
+                    case AGUA:
+                    case FOGO:
+                        return 0.5;
+                    default:
+                        return 1.0;
+                }
+            case AGUA:
+                switch (defensor) {
+                    case FOGO:
+                        return 2.0;
+                    case PLANTA:
+                    case AGUA:
+                        return 0.5;
+                    default:
+                        return 1.0;
+                }
+            case PLANTA:
+                switch (defensor) {
+                    case AGUA:
+                        return 2.0;
+                    case FOGO:
+                    case PLANTA:
+                        return 0.5;
+                    default:
+                        return 1.0;
+                }
+            case ELETRICO:
+                switch (defensor) {
+                    case VOADOR:
+                        return 2.0;
+                    case ELETRICO:
+                        return 0.5;
+                    default:
+                        return 1.0;
+                }
+            case VOADOR:
+                switch (defensor) {
+                    case PLANTA:
+                        return 2.0;
+                    case ELETRICO:
+                        return 0.5;
+                    default:
+                        return 1.0;
+                }
+            case NORMAL:
+                return 1.0; // Normal não tem efetividade especial contra nenhum tipo listado
+            default:
+                return 1.0;
+        }
     }
 }
